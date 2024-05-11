684f55c4855f1f43ee06b59fd53b8eef8796b149
angelomarangon <angelo.marangon1@icloud.com>
2024-05-11 17:52:02 +0200

    Correcciones responsive y funciones del menu y submenu

---
+++ assets/js/menu.js
@@ -0,0 +1,34 @@
+
+
+let mobile_btn = document.querySelector('.navbar__mobile-btn');
+let mobile__menu = document.querySelector('.navbar__mobile-list');
+
+
+mobile_btn.addEventListener('click', () =>{
+
+    mobile__menu.classList.toggle('menu__open');
+})
+
+
+
+
+
+//submenu
+
+let submenu_btn = document.querySelector('.item1');
+let submenu_btn2 = document.querySelector('.item2');
+let submenu = document.querySelector('#submenu1');
+let submenu2 = document.querySelector('#submenu2');
+
+
+submenu_btn.addEventListener('click', ()=> {
+
+    submenu.classList.toggle('menu__open');
+
+})
+
+submenu_btn2.addEventListener('click', ()=> {
+
+    submenu2.classList.toggle('menu__open');
+
+})
